971fa744ce6d2b397ab655121645c3ec577788af
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-04-02 19:11:47 +0000

    Begining

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp();
+  runApp(MaterialApp(
+    home:  ,
+  ));
+}
+
+class Quotes extends StatefulWidget {
+  const Quotes({Key? key}) : super(key: key);
+
+  @override
+  State<Quotes> createState() => _QuotesState();
+}
+
+class _QuotesState extends State<Quotes> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
 }
