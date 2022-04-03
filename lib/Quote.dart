8981db94c3596e0e352af544625963411c434284
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-04-03 16:53:41 +0000

    Begining

--- lib/Quote.dart
+++ lib/Quote.dart
@@ -1,10 +1,11 @@
 class Quote {
 
-  String text="";
-  String author="";
+  String text;
+  String author;
 
-  Quote(String text,String author){
-    this.text = text;
-    this.author = author;
+  Quote({required this. text,required this. author}){
   }
+
 }
+
+
