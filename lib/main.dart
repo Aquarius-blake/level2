b3219fd3fe63ccee007d8d0059cefe1491cc4dbb
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-04-04 19:28:29 +0000

    Begining

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class _QuotesState extends State<Quotes> {
     Quote(text: "Y3b3 soreeeee",author:" ~Lazarus"),
     Quote(text: "Fear Women",author:" ~Sampson"),
     Quote(text: "Anything that comes before the word \"but\" is bullshit ",author:" ~John Snow"),
-
+    Quote(text: "Valar Morgullis, Valar Dohaeris ",author:" ~Faceless god"),
   ];
 Widget qtemplate(Quote){
   return Card(
