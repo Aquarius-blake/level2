6fd7c70fc568aa3cb5cc0af7f896e8590f4931c6
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-04-05 19:51:18 +0000

    Begining

--- lib/pages/next.dart
+++ lib/pages/next.dart
@@ -19,7 +19,7 @@ backgroundColor: Colors.blue[200],
     child: Row(
       children:<Widget>[
         Icon(Icons.insert_emoticon),
-        SizedBox(width: 100,),
+        SizedBox(width: 40,),
         Title(
 
           color: Colors.white,
