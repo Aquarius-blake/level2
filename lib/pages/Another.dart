b858e3e094cd997e2dc16244f2ba20b1c06c44f9
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-04-15 15:09:10 +0000

    Begining

--- lib/pages/Another.dart
+++ lib/pages/Another.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:level2/Quote.dart';
-import 'package:level2/main.dart';
+
 
 class Another extends StatefulWidget {
   const Another({Key? key}) : super(key: key);
