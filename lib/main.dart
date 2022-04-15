b1167aed19ba13202a5006b0a36c825622aa8c86
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-04-15 14:36:00 +0000

    Begining

--- lib/main.dart
+++ lib/main.dart
@@ -8,11 +8,11 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Quotes() ,
-    routes: {
-      '/next': (context) => next(),
-      '/another':(context)=> Another(),
-    }
+      home: Quotes() ,
+      routes: {
+        '/next': (context) => next(),
+        '/another':(context)=> Another(),
+      }
   )
   );
 }
@@ -28,87 +28,87 @@ class Quotes extends StatefulWidget {
 class _QuotesState extends State<Quotes> {
 
   List<Quote> something=[
-   Quote(text: ")baa nanum y3 no d3 no, 3nfa bi da",author:" ~Adam"),
+    Quote(text: ")baa nanum y3 no d3 no, 3nfa bi da",author:" ~Adam"),
     Quote(text: "Y3b3 soreeeee",author:" ~Lazarus"),
     Quote(text: "Fear Women",author:" ~Sampson"),
     Quote(text: "Boys boys no be fam, nti mi diɛ mɛ allow cash-out ",author:" ~Judas Iscariot"),
     Quote(text: "Breast is life, Duna means power ",author:" ~Emmanuel Dikkoe"),
     Quote(text: "🤣🤣🤣🤣🤣🤣",author:" ~jackie"),
   ];
-Widget qtemplate(Quote){
-  return Card(
+  Widget qtemplate(Quote){
+    return Card(
       margin: EdgeInsets.fromLTRB(16.0, 16.0,16.0, 0),
-    child: Padding(
-      padding: const EdgeInsets.all(12.0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-children: <Widget>[
-Text(
-      Quote.text,
-  style: TextStyle(
-        fontSize: 20.0,
-      color: Colors.black,
-  ),
-),
-  SizedBox(height: 7.0,),
-  Text(
-      Quote.author,
-      style: TextStyle(
-        fontSize: 12.0,
-        color: Colors.black,
-        fontStyle: FontStyle.italic,
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              Quote.text,
+              style: TextStyle(
+                fontSize: 20.0,
+                color: Colors.black,
+              ),
+            ),
+            SizedBox(height: 7.0,),
+            Text(
+              Quote.author,
+              style: TextStyle(
+                fontSize: 12.0,
+                color: Colors.black,
+                fontStyle: FontStyle.italic,
 
-      ),
-  textAlign: TextAlign.end,
-  )
+              ),
+              textAlign: TextAlign.end,
+            )
 
-],
+          ],
+        ),
       ),
-    ),
-    elevation: 5.0,
-  );
-}
+      elevation: 5.0,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor:Colors.yellowAccent[100] ,
       drawer: Drawer(
-backgroundColor: Colors.yellowAccent,
+        backgroundColor: Colors.yellowAccent,
         child: Column(
 
         ),
       ),
       appBar: AppBar(
-          backgroundColor: Colors.blue[300],
+        backgroundColor: Colors.blue[300],
         title: Center(
           child: Title(
               color: Colors.white,
               child:Text(
                 "Famous Quotes",
-                  style: TextStyle(
-                  fontWeight: FontWeight.bold
+                style: TextStyle(
+                    fontWeight: FontWeight.bold
 
-              ),
-          )
-      ),
+                ),
+              )
+          ),
         ),
       ),
       body:Column(
 
         children: something.map((e) => qtemplate(e)
 
-          ).toList(),
+        ).toList(),
 
 
       ) ,
-   floatingActionButton: FloatingActionButton(
-       onPressed: (){
-         Navigator.pushNamed(context, '/next');
-       },
-       child: Icon(Icons.arrow_forward_ios_rounded),
-     
-       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (){
+          Navigator.pushNamed(context, '/next');
+        },
+        child: Icon(Icons.arrow_forward_ios_rounded),
+
+      ),
 
     );
   }
