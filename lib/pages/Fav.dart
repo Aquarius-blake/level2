24eaf6dbf68c5336648fe0cefa0c8b02f807acd5
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-04-16 00:35:38 +0000

    Minor update

---
+++ lib/pages/Fav.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Fav extends StatefulWidget {
+  const Fav({Key? key}) : super(key: key);
+
+  @override
+  State<Fav> createState() => _FavState();
+}
+
+class _FavState extends State<Fav> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+
+      ),
+    );
+  }
+}
