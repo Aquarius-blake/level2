3eb4c05c886dd118286447b02784405040b7a74e
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-04-18 16:30:43 +0000

    Minor update

--- lib/main.dart
+++ lib/main.dart
@@ -95,7 +95,7 @@ class _QuotesState extends State<Quotes> {
                   children: [
 CircleAvatar(
   backgroundImage: AssetImage('Assets/9.jpg'),
-  radius: 100.0,
+  radius: 40.0,
 )
                   ],
                 ),
