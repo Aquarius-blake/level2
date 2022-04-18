a2d5ada1fc996a5d6ed6e56d58b15cb6effd07dd
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-04-18 13:47:49 +0000

    Minor update

---
+++ lib/pages/Contact.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Contact extends StatefulWidget {
+  const Contact({Key? key}) : super(key: key);
+
+  @override
+  State<Contact> createState() => _ContactState();
+}
+
+class _ContactState extends State<Contact> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
