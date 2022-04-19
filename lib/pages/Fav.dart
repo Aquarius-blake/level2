949fa92e7a9bf7e35f26635807d3a0cd35648447
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-04-19 21:04:07 +0000

    Minor update

--- lib/pages/Fav.dart
+++ lib/pages/Fav.dart
@@ -68,69 +68,7 @@ fontWeight: FontWeight.bold,
               ),
             ),
           ),
-          Card(
-            elevation: 5.0,
-            color: Colors.blue[100],
-            margin: EdgeInsets.fromLTRB(16.0, 16.0,16.0, 0),
-            child: Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Text(
-                    "Address",
-                    style: TextStyle(
 
-                        fontStyle: FontStyle.italic,
-                        fontSize: 12.0
-                    ),
-                    textAlign: TextAlign.start,
-                  ),
-                  Text(
-                    "Ligma Balls 69 Times Street",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontStyle: FontStyle.italic,
-                        fontSize: 20.0
-                    ),
-                    textAlign: TextAlign.start,
-                  ),
-
-                ],
-              ),
-            ),
-          ),Card(
-            elevation: 5.0,
-            color: Colors.blue[100],
-            margin: EdgeInsets.fromLTRB(16.0, 16.0,16.0, 0),
-            child: Padding(
-              padding: const EdgeInsets.all(12.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Text(
-                    "Phone",
-                    style: TextStyle(
-
-                        fontStyle: FontStyle.italic,
-                        fontSize: 12.0
-                    ),
-                    textAlign: TextAlign.start,
-                  ),
-                  Text(
-                    "+27 294 374 4736",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontStyle: FontStyle.italic,
-                        fontSize: 20.0
-                    ),
-                    textAlign: TextAlign.start,
-                  ),
-
-                ],
-              ),
-            ),
-          ),
         ],
       ),
     );
