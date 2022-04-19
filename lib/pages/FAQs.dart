e46e0a2839e0808ea808dc3bcd70d89078f3b00b
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-04-19 20:38:00 +0000

    Minor update

--- lib/pages/FAQs.dart
+++ lib/pages/FAQs.dart
@@ -11,48 +11,48 @@ class _FAQState extends State<FAQ> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-appBar: AppBar(
-  backgroundColor: Colors.blue[300],
-title: Row(
-  children: [
-    Icon(Icons.question_answer),
-    SizedBox(width:12.0 ,),
-    Text(
-        "FAQs",
-      style: TextStyle(
-fontWeight: FontWeight.bold,
-        fontStyle: FontStyle.italic,
+      appBar: AppBar(
+        backgroundColor: Colors.blue[300],
+        title: Row(
+          children: [
+            Icon(Icons.question_answer),
+            SizedBox(width:12.0 ,),
+            Text(
+              "FAQs",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
 
+              ),
+            )
+
+          ],
+        ),
       ),
-    )
-
-  ],
-),
-),
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
 
         children: [
-Card(
-  color: Colors.blue[100],
-  margin: EdgeInsets.fromLTRB(16.0, 16.0,16.0, 0),
-  child: Padding(
-    padding: const EdgeInsets.all(12.0),
-    child: Column(
-      children: [
-Text(
-      "Something",
-style: TextStyle(
-    fontStyle: FontStyle.italic,
-    fontSize: 6.0,
+          Card(
+            color: Colors.blue[100],
+            margin: EdgeInsets.fromLTRB(16.0, 16.0,16.0, 0),
+            child: Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                children: [
+                  Text(
+                    "Something",
+                    style: TextStyle(
+                      fontStyle: FontStyle.italic,
+                      fontSize: 8.0,
 
-),
-textAlign: TextAlign.start,)
-      ],
-    ),
-  ),
+                    ),
+                    textAlign: TextAlign.start,)
+                ],
+              ),
+            ),
 
-)
+          )
         ],
       ),
     );
