b86b272f29d718b1d1d3d9fc8e0f39c9ab1179e3
Aquarius-blake <55807270+Aquarius-blake@users.noreply.github.com>
2022-04-19 20:55:44 +0000

    Minor update

--- lib/pages/Contact.dart
+++ lib/pages/Contact.dart
@@ -37,7 +37,7 @@ Card(
   child: Padding(
     padding: const EdgeInsets.all(12.0),
     child: Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
         Text(
             "Email",
